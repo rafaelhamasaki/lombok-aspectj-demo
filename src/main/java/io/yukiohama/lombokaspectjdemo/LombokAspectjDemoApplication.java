1ffec17212766637123453ffd0b869aba395bd7c
Rafael Yukio Hamasaki <rafael.hamasaki@ebanx.com>
2022-05-11 18:38:36 -0300

    - Removed unnecessary and incorrect weaveDependencies section in aspectj-maven-plugin configuration; - Replaced tabs with spaces; - Added integration test

--- src/main/java/io/yukiohama/lombokaspectjdemo/LombokAspectjDemoApplication.java
+++ src/main/java/io/yukiohama/lombokaspectjdemo/LombokAspectjDemoApplication.java
@@ -8,13 +8,13 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 @SpringBootApplication
 public class LombokAspectjDemoApplication extends SpringBootServletInitializer {
 
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(LombokAspectjDemoApplication.class);
-	}
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(LombokAspectjDemoApplication.class);
+    }
 
-	public static void main(String[] args) {
-		SpringApplication.run(LombokAspectjDemoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(LombokAspectjDemoApplication.class, args);
+    }
 
 }
--- src/test/java/io/yukiohama/lombokaspectjdemo/LombokAspectjDemoApplicationTests.java
+++ src/test/java/io/yukiohama/lombokaspectjdemo/LombokAspectjDemoApplicationTests.java
@@ -1,13 +1,22 @@
 package io.yukiohama.lombokaspectjdemo;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class LombokAspectjDemoApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void postTest(@Autowired MockMvc mvc) throws Exception {
+        mvc.perform(post("/")).andExpect(status().isOk()).andExpect(content().string("{\"id\":1}"));
+    }
 
 }
