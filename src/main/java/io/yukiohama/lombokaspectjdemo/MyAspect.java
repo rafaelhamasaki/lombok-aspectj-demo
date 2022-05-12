744275e9de05fbbe7019d6debf200c83814d9cd2
Rafael Yukio Hamasaki <rafael.hamasaki@ebanx.com>
2022-05-12 11:48:40 -0300

    Added lombok logger to Aspect to see if it works

--- src/main/java/io/yukiohama/lombokaspectjdemo/MyAspect.java
+++ src/main/java/io/yukiohama/lombokaspectjdemo/MyAspect.java
@@ -4,12 +4,15 @@ import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 
+import lombok.extern.slf4j.Slf4j;
+
 @Aspect
+@Slf4j
 public class MyAspect {
 
     @Around("@annotation(org.springframework.web.bind.annotation.PostMapping) && execution(* *(..))")
     public Object myAspect(ProceedingJoinPoint joinPoint) throws Throwable {
-        System.out.println("MyAspect!!!");
+        log.info("Aspect is working");
         return joinPoint.proceed();
     }
 }
